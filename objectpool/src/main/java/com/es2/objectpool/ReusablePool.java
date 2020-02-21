04a8c91cbd3e2822d1cdded6f087c14af892d036
Breno Salles <business@brenosalles.com>
2020-02-21 15:39:36 +0000

    Added JUnit Tests

---
+++ objectpool/src/main/java/com/es2/objectpool/ReusablePool.java
@@ -0,0 +1,75 @@
+package com.es2.objectpool;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class ReusablePool {
+    // Attributes
+    private static ReusablePool instance;
+    private Integer maxSize;
+    private ArrayList<HttpURLConnection> free;
+    private ArrayList<HttpURLConnection> used;
+
+    // Constructor
+    private ReusablePool() {
+        // Sets maxSize to default 10
+        this.maxSize = 10;
+        this.free = new ArrayList<HttpURLConnection>();
+        this.used = new ArrayList<HttpURLConnection>();
+    };
+
+    // Getters & Setters
+    public static ReusablePool getInstance() {
+        if (instance == null) {
+            instance = new ReusablePool();
+        }
+        return instance;
+    }
+
+    public synchronized HttpURLConnection acquire() throws PoolExhaustedException, IOException {
+        if (used.size() >= maxSize) {
+            // Atingiu-se limite de connections
+            throw new PoolExhaustedException();
+        }
+
+        HttpURLConnection outConn = null;
+        if (free.isEmpty() == true) {
+            // Não existem mais ligações disponíveis
+            outConn = (HttpURLConnection) new URL("http://ipv.pt").openConnection();
+        } else {
+            // Usar uma ligação livre
+            outConn = free.get(0);
+            free.remove(outConn);
+            outConn.connect();
+        }
+
+        used.add(outConn);
+        return outConn;
+    }
+
+    public synchronized void release(HttpURLConnection conn) throws ObjectNotFoundException {
+        if (used.contains(conn) == false) {
+            throw new ObjectNotFoundException();
+        }
+        conn.disconnect();
+        used.remove(conn);
+        free.add(conn);
+    }
+
+    public synchronized void resetPool() {
+        for (HttpURLConnection connection : free) {
+            connection.disconnect();
+        }
+        free.clear();
+        for (HttpURLConnection connection : used) {
+            connection.disconnect();
+        }
+        used.clear();
+    }
+
+    public synchronized void setMaxPoolSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+}
---
+++ objectpool/src/test/java/com/es2/objectpool/ReusablePoolTest.java
@@ -0,0 +1,79 @@
+package com.es2.objectpool;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ReusablePoolTest {
+    private static ReusablePool pool;
+
+    @BeforeClass
+    public static void beforeClass() {
+        pool = ReusablePool.getInstance();
+    }
+
+    @Before
+    public void suitUp() {
+        pool.resetPool();
+    }
+
+    @After
+    public void tearDown() {
+        pool.resetPool();
+    }
+
+    @Test
+    public void testNullReusablePool() {
+        assertFalse("ReusablePool is null!", pool == null);
+    }
+
+    @Test(expected = PoolExhaustedException.class)
+    public void testMaxConnectionsInitialized() throws PoolExhaustedException, IOException {
+        Integer maxConnections = 10;
+
+        HttpURLConnection conn = null;
+        for (Integer i = 0; i < maxConnections; i++) {
+            conn = pool.acquire();
+            assertFalse("Max connections not set properly!", conn == null);
+            conn = null;
+        }
+
+        conn = pool.acquire();
+        assertTrue("Max connections not set properly!", conn == null);
+    }
+
+    @Test(expected = PoolExhaustedException.class)
+    public void testSetMaximumConnections() throws PoolExhaustedException, IOException {
+        Integer maxConnections = (int) (Math.random() * 20);
+
+        HttpURLConnection conn = null;
+        for (Integer i = 0; i < maxConnections; i++) {
+            conn = pool.acquire();
+            assertFalse("Max connections could not be set properly!", conn == null);
+            conn = null;
+        }
+        conn = pool.acquire();
+        assertFalse("Max connections could not be set properly!", conn == null);
+    }
+
+    @Test(expected = ObjectNotFoundException.class)
+    public void testObjectNullNotFound() throws ObjectNotFoundException {
+        HttpURLConnection conn = null;
+        pool.release(conn);
+    }
+
+    @Test(expected = ObjectNotFoundException.class)
+    public void testObjectNotFound() throws ObjectNotFoundException, PoolExhaustedException, IOException {
+        HttpURLConnection conn = pool.acquire();
+        assertFalse("Connection is not returning!", conn == null);
+        pool.release(conn);
+        pool.release(conn);
+    }
+}
