6a4ddafeb8ad14d05bffc74e6b690bc022f6f5b0
Breno Salles <business@brenosalles.com>
2020-02-27 17:27:29 +0000

    Started bridge

---
+++ bridge/src/main/java/com/es2/bridge/ServiceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.es2.bridge;
+
+/**
+ * Hello world!
+ *
+ */
+public class ServiceNotFoundException {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
