6cb18c26b06008394f0bd621e9c71274e3aa32c3
Breno Salles <business@brenosalles.com>
2020-02-28 15:25:39 +0000

    Started Composite

---
+++ composite/src/test/java/com/es2/composite/CompositeTest.java
@@ -0,0 +1,12 @@
+package com.es2.composite;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class CompositeTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
